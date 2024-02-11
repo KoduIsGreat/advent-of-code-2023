c7f2c0ff1e6a38f4bbdcd05a027df64374fdb8ef
Adam Shelton <adamshelton@rti.org>
2024-02-11 11:44:21 -0500

    1part1

--- python/1/main.py
+++ python/1/main.py
@@ -1,1 +1,28 @@
-print('lets get started!')
+input = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+
+
+def find_answer_part_1(input: str) -> int:
+    lines = input.split("\n")
+    sum = 0
+    for i in lines:
+        if i == "":
+            continue
+        nums_only = "".join([j for j in i if not j.isalpha()])
+        le = len(nums_only)
+        sum += int(nums_only[0])*10
+        if le == 0:
+            sum += int(nums_only[0])
+        else:
+            sum += int(nums_only[le-1])
+    return sum
+
+
+sample_ans = find_answer_part_1(input)
+print("Sample Answer: ", sample_ans)
+
+with open("1/input.txt") as file:
+    input = file.read()
+    print("Answer: ", find_answer_part_1(input))
